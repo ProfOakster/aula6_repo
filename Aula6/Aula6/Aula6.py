c0b61ad4dd9876b8e3a5f92eba8b8fe55074b40e
The0akster <2606tom@gmail.com>
2022-11-03 16:52:35 +0000

    Implemented Main Game Loop

--- Aula6/Aula6/Aula6.py
+++ Aula6/Aula6/Aula6.py
@@ -4,22 +4,23 @@ import random
 deck_player=["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
 deck_npc=["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
 
-def shuffle():
+def shuffle(): #shuffles both decks
     random.shuffle(deck_player)
     random.shuffle(deck_npc)
 
 
-def show():
+def show(): #prints both decks' contents
     print("Player Deck:",deck_player)
     print("NPC Deck", deck_npc)
     
-def fish(deck):
+def fish(deck): #returns the top 2 cards of the inputted deck
     top1=deck[-1]
     top2=deck[-2]
-    return top1, top2
+    hand=[top1,top2]
+    return hand
    
 
-def value(card):
+def value(card): #returns the point value of the inputted card
     if card =="J":
         return 11
     elif card =="Q":
@@ -33,12 +34,10 @@ def value(card):
 
 
 
-def compare():
-    top_player=fish(deck_player)
-    top_npc=fish(deck_npc)
+def compare(hand_player,hand_npc): #returns 1,-1 or 0 when the player's 2 cards are worth more, less or the same as the npc's
     
-    pnt_player=value(top_player[0])+value(top_player[1])
-    pnt_npc=value(top_npc[0])+value(top_npc[1])
+    pnt_player=value(hand_player[0])+value(hand_player[1])
+    pnt_npc=value(hand_npc[0])+value(hand_npc[1])
 
     if pnt_player>pnt_npc:
         return 1
@@ -48,13 +47,43 @@ def compare():
         return 0
 
 
+inp="debug"
+while inp!="n":
+    shuffle()
+    hand_player=fish(deck_player)
+    print("Your hand:",hand_player)
+    print("1)Discard both cards")
+    print("2)Discard 1 card")
+    print("3)Keep current hand")
+    inp=input("Select option: (1/2/3):")
+    if inp=="1":
+        hand_player=(deck_player[-3],deck_player[-4])
+    elif inp=="2":
+        inp=input("Discard first or second card? (1/2):")
+        if inp=="1":
+            hand_player=[deck_player[-3],hand_player[1]]
+        elif inp=="2":
+            hand_player=[hand_player[0],deck_player[-3]]
+    
+
+    hand_npc=fish(deck_npc)
+    print("Your hand:",hand_player)
+    print("Opponent's hand:",hand_npc)
+    res=compare(hand_player,hand_npc)
+    
+    if res==1:
+        print("Winner!")
+    elif res==-1:
+        print("You lose")
+    else:
+        print("Draw")
+
+    inp=input("Play again? (y/n):")
 
 
-shuffle()
-show()
-print(fish(deck_player))
-print(fish(deck_npc))
-print(compare())
+
+
+
 
 
 
